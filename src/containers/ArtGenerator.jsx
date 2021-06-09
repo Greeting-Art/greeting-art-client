377cd3de12cedfb00f62e9cf589283a76a6c7028
SamuelHGerber <henrylightfoot@gmail.com>
2021-06-09 11:57:41 -0700

    added styling to ArtGenerator container

--- src/containers/ArtGenerator.jsx
+++ src/containers/ArtGenerator.jsx
@@ -1,14 +1,41 @@
 /* eslint-disable indent */
-import React, {useState} from 'react';
+import React, { useState } from 'react';
+import styles from './ArtGenerator.css';
 
 export default function ArtGenerator() {
-    const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(false);
 
-    if (loading) return <h2>loading...</h2>;
+  if (loading) return <h2>loading...</h2>;
 
-    return (
-        <div>
-            <h1>Art Generator</h1>
+  return (
+    // <div>
+    //     <h1>Art Generator</h1>
+    // </div>
+
+    <main className={styles.artPage}>
+      <section className={styles.artHeader}>
+        <h1>Art Generator</h1>
+      </section>
+
+      <section className={styles.artBody}>
+        <div className={styles.controlBar}></div>
+        <div className={styles.artBar}>
+          <div className={styles.leftColumn}>
+            <p>left column</p>
+          </div>
+          <div className={styles.centerColumn}>
+            <figure className={styles.canvasWrapper}>
+              <img src="https://i.ytimg.com/vi/PXye-6_ZB1w/hqdefault.jpg" />
+            </figure>
+          </div>
+          <div className={styles.rightColumn}>
+            <button>button</button>
+          </div>
         </div>
-    );
+        <div className={styles.lowerBar}>
+          <p>lower margin</p>
+        </div>
+      </section>
+    </main>
+  );
 }
