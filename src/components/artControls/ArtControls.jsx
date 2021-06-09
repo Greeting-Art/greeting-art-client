494af7202987f46b591cf8f61a7b819f88ca7808
Stephen Tamiesie <stephen.tamiesie@gmail.com>
2021-06-09 10:28:36 -0700

    component architecture

---
+++ src/components/artControls/ArtControls.jsx
@@ -0,0 +1,18 @@
+/* eslint-disable indent */
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function ArtControls(props) {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+ArtControls.propTypes = {
+
+};
+
+export default ArtControls;
+
---
+++ src/components/galleryList/GalleryList.jsx
@@ -0,0 +1,18 @@
+/* eslint-disable indent */
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function GalleryList(props) {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+GalleryList.propTypes = {
+
+};
+
+export default GalleryList;
+
