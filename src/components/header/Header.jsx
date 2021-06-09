967735babeba7d47a29b4b36afc30d45408f1eb0
SamuelHGerber <henrylightfoot@gmail.com>
2021-06-09 11:28:50 -0700

    header links working and formatted

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,18 +1,28 @@
 /* eslint-disable indent */
 import React from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import styles from './Header.css';
 
 function Header(props) {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <div className={styles.linkWrapper}>
+      <Link to="/">
+        <h3>Home</h3>
+      </Link>
+      <Link to="/ecard">
+        <h3>ECard</h3>
+      </Link>
+      <Link to="/gallery">
+        <h3>Gallery</h3>
+      </Link>
+      <Link to="/aboutus">
+        <h3>About Us</h3>
+      </Link>
+    </div>
+  );
 }
 
-Header.propTypes = {
-
-};
+Header.propTypes = {};
 
 export default Header;
-
