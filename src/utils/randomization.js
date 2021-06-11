98d666b66dd0cadeba9f936909e7a51ae3629ce7
SamuelHGerber <henrylightfoot@gmail.com>
2021-06-11 16:04:32 -0700

    sketch logic added from sandbox

--- src/utils/randomization.js
+++ src/utils/randomization.js
@@ -20,3 +20,80 @@ export const randomArt = () => {
   };
   return sketch;
 };
+
+const randomizeRatio = () => {
+  return Math.round(Math.random() * 80);
+};
+
+const randomizeColor = () => {
+  return Math.round(Math.random() * 254);
+};
+
+const randomizeCurve = () => {
+  return Math.round(Math.random() * 640);
+};
+
+export const weirdArt = () => {
+  const sketch = (p5) => {
+    p5.setup = () => {
+      p5.createCanvas(560, 560);
+      // p5.noStroke();
+      p5.stroke(randomizeColor(), randomizeColor(), randomizeColor());
+      // p5.background(randomizeColor(), randomizeColor(), randomizeColor());
+      p5.background(randomizeColor(), randomizeColor(), randomizeColor());
+      //p5.erase(randomizeColor(), randomizeColor(), randomizeColor());
+      //p5.arc(50, 50, randomizeRatio(), randomizeRatio(), 2, 5);
+      p5.bezier(
+        randomizeCurve(),
+        randomizeCurve(),
+        randomizeCurve(),
+        randomizeCurve(),
+        randomizeCurve(),
+        randomizeCurve(),
+        randomizeCurve(),
+        randomizeCurve()
+      );
+
+      p5.bezier(
+        randomizeCurve(),
+        randomizeCurve(),
+        randomizeCurve(),
+        randomizeCurve(),
+        randomizeCurve(),
+        randomizeCurve(),
+        randomizeCurve(),
+        randomizeCurve()
+      );
+
+      const newDensity = randomizeRatio();
+      const newColor = randomizeColor();
+
+      for (let i = 0; i < 1000; i++) {
+        p5.fill(
+          p5.random(newColor),
+          p5.random(newColor),
+          p5.random(newColor),
+          p5.random(newColor)
+        );
+        p5.square(
+          p5.random(p5.windowWidth),
+          p5.random(p5.windowHeight),
+          p5.random(newDensity)
+        );
+        p5.circle(
+          p5.random(p5.windowWidth),
+          p5.random(p5.windowHeight),
+          p5.random(newDensity)
+        );
+        p5.triangle(
+          p5.random(p5.windowWidth),
+          p5.random(p5.windowHeight),
+          p5.random(newDensity)
+        );
+      }
+      p5.textSize(randomizeRatio());
+      p5.text('wave', randomizeColor(), randomizeColor());
+    };
+  };
+  return sketch;
+};
