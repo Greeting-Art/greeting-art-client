176bb03a6cb89d6dc4f956e446e8433e2bd44d4b
phsequeira <phsequeira@yahoo.com>
2021-06-11 16:30:39 -0700

    Added Emailing service to Ecard

--- src/containers/ECard.jsx
+++ src/containers/ECard.jsx
@@ -1,13 +1,44 @@
 /* eslint-disable indent */
 import React, { useState, useEffect } from 'react';
 import styles from './ECard.css';
+import { sendMsg } from '../utils/sendGridMessage';
 
 export default function ECard() {
   const [loading, setLoading] = useState(true);
+  const [disabled, setDisabled] = useState(true);
+  const [emailSent, setSent] = useState('');
+  const [formData, setForm] = useState({
+    email: '',
+    senderName: '',
+    message: ''
+  });
 
   useEffect(() => {
     setLoading(false);
   }, []);
+
+  useEffect(() => {
+    if (formData.email.length > 1 && formData.senderName.length > 1) {
+      setDisabled(false)
+    }
+    else {
+      setDisabled(true)
+    }
+  }, [formData.email, formData.senderName])
+
+  const handleForm = (e) => {
+    setForm({
+      ...formData,
+      [e.target.name]: e.target.value
+    })
+  }
+
+  const handleSumbit = async (e) => {
+    e.preventDefault()
+    await fetch('http://localhost:7890/send', sendMsg(formData))
+      .then(res => res.json())
+      .then(setSent('You have sent an email successfully'))
+  }
 
   if (loading) return <h2>loading...</h2>;
 
@@ -28,15 +59,41 @@ export default function ECard() {
           </figure>
         </div>
         <div className={styles.eCardForm}>
-          <input type="text" value="email"></input>
-          <input type="date" value="2021-06-10"></input>
-          <textarea rows="4" cols="50">
-            message
-          </textarea>
-          <button>submit</button>
+          <form>
+            <label>Recipient Email: </label>
+            <input
+              type='email'
+              name='email'
+              value={formData.email}
+              onChange={handleForm}
+            />
+            <br /><br />
+            <label>Your Name: </label>
+            <input
+              type='text'
+              name='senderName'
+              value={formData.senderName}
+              onChange={handleForm}
+            />
+            <br /><br />
+            <label>Message (optional): </label>
+            <br />
+            <textarea
+              name='message'
+              value={formData.message}
+              onChange={handleForm}
+            />
+            <br /><br />
+            <button
+              disabled={disabled}
+              onClick={handleSumbit}>Send</button>
+          </form>
         </div>
       </section>
-      <section className={styles.eCardFooter}>footer</section>
+      <section className={styles.eCardFooter}>footer
+        <br />
+        <h1>{emailSent}</h1>
+      </section>
     </main>
   );
 }
