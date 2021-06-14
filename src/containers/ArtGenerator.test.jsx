1dfb5fdc80fe58e6aa40e7197961d8649eea407b
SamuelHGerber <henrylightfoot@gmail.com>
2021-06-14 16:16:21 -0700

    ArtGenerator test adjusted for mock

--- src/containers/ArtGenerator.test.jsx
+++ src/containers/ArtGenerator.test.jsx
@@ -5,25 +5,13 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import App from '../components/app/App';
-// import ArtGenerator from './ArtGenerator';
 
+//this needs to be expanded once ArtGenerator is finalized as a container:
 jest.mock('./ArtGenerator', () => () => <div>hey</div>);
-
-//// for ref -- here is what <P5Wrapper /> console logs as:
-// const P5Wrapper = function P5Wrapper(props) {
-//   var _this = _super.call(this, props) || this;
-//   _this.wrapper = null;
-//   _this.state = __assign(__assign({}, props), {
-//     canvas: null,
-//     wrapper: _this.wrapper,
-//   });
-//   return _this;
-// };
 
 describe('random art generation container', () => {
   it('displays inputs + an image + buttons', async () => {
-    // render(<ArtGenerator />);
-
+    //// this loading test will not work as long as we are mocking ArtGenerator:
     // const screenLoading = await screen.getByText('loading...');
     // expect(screenLoading).toMatchSnapshot();
 
