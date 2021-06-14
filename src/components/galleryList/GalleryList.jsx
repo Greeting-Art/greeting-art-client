3fd31fcb7f234472cba48e36162569bda00e1496
Eion Nelson <ecnelson1@live.com>
2021-06-14 11:55:34 -0700

    added the GalleryList component

--- src/components/galleryList/GalleryList.jsx
+++ src/components/galleryList/GalleryList.jsx
@@ -3,14 +3,26 @@ import React from 'react';
 import GalleryDetail from '../galleryDetail/GalleryDetail';
 import PropTypes from 'prop-types';
 
-function GalleryList(props) {
-  return (
-    <div>
-      <GalleryDetail />
-    </div>
-  );
+function GalleryList({Contents}) {
+    console.log('Contents in Gallery', Contents)
+    return (
+        <ul>
+            {Contents.map((artWork) => 
+                <li key={artWork.Key}>
+                    <Art Key={artWork.Key}/>
+                </li>
+            )}
+            
+        </ul>
+    )
 }
 
-GalleryList.propTypes = {};
+GalleryWalls.propTypes = {
+   Contents: PropTypes.arrayOf(
+    PropTypes.shape({
+      Key: PropTypes.string.isRequired,
+    })
+  ).isRequired,
+};
 
-export default GalleryList;
+export default GalleryList
