2dac3e843a068914e74b010aef93885a8b989675
SamuelHGerber <henrylightfoot@gmail.com>
2021-06-15 06:44:40 -0700

    material-ui menu added to Header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,24 +1,63 @@
 /* eslint-disable indent */
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import Button from '@material-ui/core/Button';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
 import styles from './header.css';
 
 function Header() {
+  const [anchorEl, setAnchorEl] = useState(null);
+
+  const handleMenuClick = (e) => {
+    setAnchorEl(e.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
-    <div className={styles.linkWrapper}>
-      <Link to="/">
-        <h3>Home</h3>
-      </Link>
-      <Link to="/ecard">
-        <h3>ECard</h3>
-      </Link>
-      <Link to="/gallery">
-        <h3>Gallery</h3>
-      </Link>
-      <Link to="/aboutus">
-        <h3>About Us</h3>
-      </Link>
-    </div>
+    <section className={styles.fullHeader}>
+      <div className={styles.linkBar}>
+        <IconButton
+          // edge="start"
+          color="inherit"
+          aria-label="menu"
+          aria-controls="simple-menu"
+          aria-haspopup="true"
+          onClick={handleMenuClick}
+          className="iconButtonMUI"
+        >
+          <MenuIcon fontSize="large" />
+        </IconButton>
+        <Menu
+          id="simple-menu"
+          anchorEl={anchorEl}
+          keepMounted
+          open={Boolean(anchorEl)}
+          onClose={handleClose}
+        >
+          <MenuItem onClick={handleClose} component={Link} to="/">
+            Home
+          </MenuItem>
+          <MenuItem onClick={handleClose} component={Link} to="/gallery">
+            Gallery
+          </MenuItem>
+          <MenuItem onClick={handleClose} component={Link} to="/ecard">
+            ECard
+          </MenuItem>
+          <MenuItem onClick={handleClose} component={Link} to="/aboutus">
+            About Us
+          </MenuItem>
+        </Menu>
+      </div>
+      <div className={styles.titleBar}>
+        <p>Greeting Art</p>
+      </div>
+    </section>
   );
 }
 
