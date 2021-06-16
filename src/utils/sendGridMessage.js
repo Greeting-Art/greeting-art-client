1e89aa936bce08bd3d8d7dd4682ab9f9de7dfb92
phsequeira <phsequeira@yahoo.com>
2021-06-16 11:57:05 -0700

    pull

--- src/utils/sendGridMessage.js
+++ src/utils/sendGridMessage.js
@@ -1,6 +1,7 @@
-import logo from '.././assets/3D13-greeting-art-transparent.png'
 function sendMsg(formData, artId) {
 
+
+  //change url to deployed sites
     return {
         method: 'POST',
         headers: {
@@ -13,7 +14,6 @@ function sendMsg(formData, artId) {
             'to': `${formData.email}`,
             'from': 'GreetingArt@outlook.com',
             'subject': `${formData.senderName} has send you a Greeting Art`,
-            //'send_at': radio,
             'html': `<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
         <html data-editor-version="2" class="sg-campaigns" xmlns="http://www.w3.org/1999/xhtml">
             <head>
@@ -167,7 +167,7 @@ function sendMsg(formData, artId) {
             </tbody>
           </table><table class="wrapper" role="module" data-type="image" border="0" cellpadding="0" cellspacing="0" width="100%" style="table-layout: fixed;" data-muid="uXsDxMnn1bRMmDcX8NB6rW">
               <tbody><tr>
-                <td style="font-size:6px; line-height:10px; padding:30px 0px 30px 0px;" bgcolor="#000000" valign="top" align="center"><img class="max-width" border="0" style="display:block; color:#000000; text-decoration:none; font-family:Helvetica, arial, sans-serif; font-size:16px; max-width:50% !important; width:50%; height:auto !important;" src="${logo}" alt="SongRiddle" width="300" data-responsive="true" data-proportionally-constrained="false"></td>
+                <td style="font-size:6px; line-height:10px; padding:30px 0px 30px 0px;" bgcolor="#000000" valign="top" align="center"><img class="max-width" border="0" style="display:block; color:#000000; text-decoration:none; font-family:Helvetica, arial, sans-serif; font-size:16px; max-width:50% !important; width:50%; height:auto !important;" src="https://github.com/Greeting-Art/greeting-art-client/blob/dev/src/assets/3D13-greeting-art-transparent.png?raw=true" alt="SongRiddle" width="300" data-responsive="true" data-proportionally-constrained="false"></td>
               </tr>
             </tbody></table><table class="module" role="module" data-type="text" border="0" cellpadding="0" cellspacing="0" width="100%" style="table-layout: fixed;" data-muid="hL6wjQ2qknNd5qDwT1p7Up" data-mc-module-version="2019-10-22">
               <tbody><tr>
@@ -175,11 +175,11 @@ function sendMsg(formData, artId) {
               <tbody>
             </tbody></table><table class="module" role="module" data-type="text" border="0" cellpadding="0" cellspacing="0" width="100%" style="table-layout: fixed;" data-muid="qk51Jjn4bm3rn2Yb31Dxzb" data-mc-module-version="2019-10-22">
               <tbody><tr>
-                <td style="background-color:#ffffff; padding:50px 50px 10px 50px; line-height:22px; text-align:center;" height="100%" valign="top" bgcolor="#ffffff"><div><div style="font-family: inherit; text-align: inherit"><span style="font-size: 24px; font-family: verdana, geneva, sans-serif"><strong>You just recieved a Greeting Card from Greeters!.</strong></span></div><div></div></div></td>
+                <td style="background-color:#ffffff; padding:50px 50px 10px 50px; line-height:22px; text-align:center;" height="100%" valign="top" bgcolor="#ffffff"><div><div style="font-family: inherit; text-align: inherit"><span style="font-size: 24px; font-family: verdana, geneva, sans-serif"><strong>You just recieved an Ecard from Greeting Art!.</strong></span></div><div></div></div></td>
               </tr>
             </tbody></table><table class="module" role="module" data-type="text" border="0" cellpadding="0" cellspacing="0" width="100%" style="table-layout: fixed;" data-muid="iTBXe9c6QUCujvmJs8hYKr" data-mc-module-version="2019-10-22">
               <tbody><tr>
-                <td style="background-color:#ffffff; padding:40px 40px 40px 40px; line-height:22px; text-align:inherit;" height="100%" valign="top" bgcolor="#ffffff"><div><div style="font-family: inherit; text-align: inherit"><span style="font-size: 16px; font-family: verdana, geneva, sans-serif">Someone must love you! Or at least like you enough to send you a greenting card.</span></div>
+                <td style="background-color:#ffffff; padding:40px 40px 40px 40px; line-height:22px; text-align:inherit;" height="100%" valign="top" bgcolor="#ffffff"><div><div style="font-family: inherit; text-align: inherit"><span style="font-size: 16px; font-family: verdana, geneva, sans-serif">${formData.senderName} must love you! Or at least like you enough to send you a greenting card.</span></div>
         <div style="font-family: inherit; text-align: inherit">&nbsp;</div>
         <div></div></div></td>
               </tr><tr>
@@ -190,7 +190,7 @@ function sendMsg(formData, artId) {
         <div style="font-family: inherit; text-align: inherit">&nbsp;</div>
         <div></div></div></td>
               </tr>
-            </tbody></table><table border="0" cellpadding="0" cellspacing="0" class="module" data-role="module-button" data-type="button" role="module" style="table-layout:fixed" width="100%" data-muid="qY8ouFUf6bFVP8tHkQ5gq7"><tbody><tr><td align="center" bgcolor="#ffffff" class="outer-td" style="padding:20px 20px 60px 20px; background-color:#ffffff;"><table border="0" cellpadding="0" cellspacing="0" class="button-css__deep-table___2OZyb wrapper-mobile" style="text-align:center"><tbody><tr><td align="center" bgcolor="#00dc73" class="inner-td" style="border-radius:6px; font-size:16px; text-align:center; background-color:inherit;"><a style="background-color:#00dc73; border:0px solid #08b65d; border-color:#08b65d; border-radius:0px; border-width:0px; color:#ffffff; display:inline-block; font-family:verdana,geneva,sans-serif; font-size:16px; font-weight:normal; letter-spacing:3px; line-height:30px; padding:12px 18px 12px 18px; text-align:center; text-decoration:none; border-style:solid;" href="https://github.com/Greeting-Art" target="_blank">Make Your Own Greeting Card</a></td></tr></tbody></table></td></tr></tbody></table>
+            </tbody></table><table border="0" cellpadding="0" cellspacing="0" class="module" data-role="module-button" data-type="button" role="module" style="table-layout:fixed" width="100%" data-muid="qY8ouFUf6bFVP8tHkQ5gq7"><tbody><tr><td align="center" bgcolor="#ffffff" class="outer-td" style="padding:20px 20px 60px 20px; background-color:#ffffff;"><table border="0" cellpadding="0" cellspacing="0" class="button-css__deep-table___2OZyb wrapper-mobile" style="text-align:center"><tbody><tr><td align="center" bgcolor="#5471CF" class="inner-td" style="border-radius:6px; font-size:16px; text-align:center; background-color:inherit;"><a style="background-color:#5471CF; border:0px solid #08b65d; border-color:#08b65d; border-radius:0px; border-width:0px; color:#ffffff; display:inline-block; font-family:verdana,geneva,sans-serif; font-size:16px; font-weight:normal; letter-spacing:3px; line-height:30px; padding:12px 18px 12px 18px; text-align:center; text-decoration:none; border-style:solid;" href="https://github.com/Greeting-Art" target="_blank">Make Your Own Greeting Card</a></td></tr></tbody></table></td></tr></tbody></table>
             <table class="module" role="module" data-type="spacer" border="0" cellpadding="0" cellspacing="0" width="100%" style="table-layout: fixed;" data-muid="2ga5f7koD5ApvUfnqUK6aT">
               <tbody><tr>
                 <td style="padding:0px 0px 30px 0px;" role="module-content" bgcolor="#000000">
@@ -201,7 +201,7 @@ function sendMsg(formData, artId) {
                 <td style="padding:0px 0px 0px 0px;" role="module-content" height="100%" valign="top" bgcolor="#000000">
                   <table border="0" cellpadding="0" cellspacing="0" align="center" width="100%" height="3px" style="line-height:3px; font-size:3px;">
                     <tbody><tr>
-                      <td style="padding:0px 0px 3px 0px;" bgcolor="#42ee99"></td>
+                      <td style="padding:0px 0px 3px 0px;" bgcolor="#5471CF"></td>
                     </tr>
                   </tbody></table>
                 </td>
@@ -220,7 +220,7 @@ function sendMsg(formData, artId) {
                 <td style="padding:0px 0px 30px 0px;" role="module-content" bgcolor="#000000">
                 </td>
               </tr>
-            </tbody></table><div data-role="module-unsubscribe" class="module unsubscribe-css__unsubscribe___2CDlR" role="module" data-type="unsubscribe" style="background-color:#000000; color:#ffffff; font-size:12px; line-height:20px; padding:16px 16px 16px 16px; text-align:center;" data-muid="vqq9VGfbTmLsFG2U3YC8Fh"><p style="font-family:verdana,geneva,sans-serif; font-size:12px; line-height:20px;"><a class="aboutUs--aboutUsLink" href="https://github.com/Greeting-Art" style="color:#42ee99;">About Us</a></p></div><table class="module" role="module" data-type="spacer" border="0" cellpadding="0" cellspacing="0" width="100%" style="table-layout: fixed;" data-muid="vKe4rfbECMPFgNz27Wg5EW">
+            </tbody></table><div data-role="module-unsubscribe" class="module unsubscribe-css__unsubscribe___2CDlR" role="module" data-type="unsubscribe" style="background-color:#000000; color:#ffffff; font-size:12px; line-height:20px; padding:16px 16px 16px 16px; text-align:center;" data-muid="vqq9VGfbTmLsFG2U3YC8Fh"><p style="font-family:verdana,geneva,sans-serif; font-size:12px; line-height:20px;"><a class="aboutUs--aboutUsLink" href="https://github.com/Greeting-Art" style="color:#5471CF;">About Us</a></p></div><table class="module" role="module" data-type="spacer" border="0" cellpadding="0" cellspacing="0" width="100%" style="table-layout: fixed;" data-muid="vKe4rfbECMPFgNz27Wg5EW">
               <tbody><tr>
                 <td style="padding:0px 0px 30px 0px;" role="module-content" bgcolor="#000000">
                 </td>
