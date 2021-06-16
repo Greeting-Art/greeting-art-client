a2a50a768f4f87a8ff588334f2eafe920d038154
Stephen Tamiesie <stephen.tamiesie@gmail.com>
2021-06-16 11:09:09 -0700

    gallery sort function implemented

--- src/containers/Gallery.jsx
+++ src/containers/Gallery.jsx
@@ -1,17 +1,19 @@
+/* eslint-disable max-len */
 /* eslint-disable indent */
 import React, { useState, useEffect } from 'react';
 import styles from './Gallery.css';
 import GalleryList from '../components/galleryList/GalleryList';
 import { getGallery } from '../utils/s3-utils';
 import CircularProgress from '@material-ui/core/CircularProgress';
+import { sortGallery } from '../utils/utils';
 
 export default function Gallery() {
   const [loading, setLoading] = useState(true);
-  const [Contents, setContents] = useState([]);
+  const [Contents, setContents] = useState([]);  
 
   useEffect(() => {
     getGallery()
-      .then((Contents) => setContents(Contents))
+    .then((Contents) => setContents(sortGallery(Contents)))
       .finally(() => setLoading(false));
   }, []);
 
