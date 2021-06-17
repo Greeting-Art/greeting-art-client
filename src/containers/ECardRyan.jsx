5933dc45a9747caaa5bc7f5a17e863b5d8b885a6
Stephen Tamiesie <stephen.tamiesie@gmail.com>
2021-06-17 14:10:42 -0700

    add ryan birthday photo to email page

--- src/containers/ECardRyan.jsx
+++ src/containers/ECardRyan.jsx
@@ -9,7 +9,7 @@ export default function ECardRyan() {
   const [disabled, setDisabled] = useState(true);
   const [emailSent, setSent] = useState(true);
   const [artId, setArtId] = useState(
-    'https://github.com/Greeting-Art/greeting-art-client/blob/dev/src/assets/appIcons/blob-ross_8f128.gif?raw=true'
+    'https://github.com/Greeting-Art/greeting-art-client/blob/feat/card-45-about-us-page-styling/src/assets/appIcons/happy-bday-ryan.png?raw=true'
   );
   const [formData, setForm] = useState({
     email: '',
@@ -66,7 +66,7 @@ export default function ECardRyan() {
         <div className={styles.eCardDisplay}>
           <figure className={styles.canvasWrapper}>
             <img
-              src={`https://github.com/Greeting-Art/greeting-art-client/blob/dev/src/assets/appIcons/blob-ross_8f128.gif?raw=true`}
+              src={`https://github.com/Greeting-Art/greeting-art-client/blob/feat/card-45-about-us-page-styling/src/assets/appIcons/happy-bday-ryan.png?raw=true`}
               height="360"
               width="360"
             />
