230d91d80f75c3ebc52b5c1d68ed8d89c4e4c2e2
Stephen Tamiesie <stephen.tamiesie@gmail.com>
2021-06-17 13:27:39 -0700

    add flex to about page and blob ross links

--- src/components/aboutUs/AboutUs.jsx
+++ src/components/aboutUs/AboutUs.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unescaped-entities */
 /* eslint-disable max-len */
 /* eslint-disable indent */
 import React from 'react';
@@ -12,8 +13,9 @@ import Stephen from '../../assets/teamPics/Stephen.jpg';
 
 function AboutUsComp() {
   return (
-    <div className={styles.teamContainer}>
+    <>
       <p className={styles.teamHeader}>About the Team</p>
+    <div className={styles.teamContainer}>
       <div className={styles.teamMembers}>
         <div className={styles.memberItem}>
         <img className={styles.memberImage} alt="Eion" src={Eion}/>
@@ -73,6 +75,37 @@ function AboutUsComp() {
             </a>
           </p>
         </div>
+
+        <div className={styles.memberItem}>
+          <img className={styles.blobImage} alt="Blob" src={Blob} />
+          <h4>Blob Ross</h4>
+          <p className={styles.devTitle}>Sr. Art Generator</p>
+          <p>
+            <a
+              href="https://www.youtube.com/watch?v=YLO7tCdBVrA"
+              target="_blank"
+              rel="noreferrer noopener"
+            >
+              <img
+                className={styles.linkedInLogo}
+                src={linkedIn}
+                alt="linkedIn"
+              />
+            </a>
+            <a
+              href="https://github.com/Greeting-Art"
+              target="_blank"
+              rel="noreferrer noopener"
+            >
+              <img
+                className={styles.githubLogo}
+                src={github}
+                alt="github"
+              />
+            </a>
+          </p>
+        </div>
+      </div>
         <div className={styles.memberItem}>
         <img className={styles.memberImage} alt="Sam" src={Sam}/>
           <h4>Sam Gerber</h4>
@@ -131,37 +164,12 @@ function AboutUsComp() {
             </a>
           </p>
         </div>
-        <div className={styles.memberItem}>
-          <img className={styles.blobImage} alt="Blob" src={Blob} />
-          <h4>Blob Ross</h4>
-          <p className={styles.devTitle}>Software Developer</p>
-          <p>
-            <a
-              href="https://www.linkedin.com/in/stephentamiesie/"
-              target="_blank"
-              rel="noreferrer noopener"
-            >
-              <img
-                className={styles.linkedInLogo}
-                src={linkedIn}
-                alt="linkedIn"
-              />
-            </a>
-            <a
-              href="https://github.com/stamiesie"
-              target="_blank"
-              rel="noreferrer noopener"
-            >
-              <img
-                className={styles.githubLogo}
-                src={github}
-                alt="github"
-              />
-            </a>
-          </p>
-        </div>
-      </div>
+        
+      <footer className={styles.aboutFooter}>
+      **This app was made using resources by p5*js, AWS, SendGrid, Material-UI, FlatIcon and 3D Thirteen Pixel and Pixel Caps fonts by 30100flo and Mirz123, respectively. Blob Ross would also like to thank PiskelApp and last but certainly not least, "Blob Master" Jerud. Happy Rendering!
+      </footer>
     </div>
+    </>
   );
 }
 
