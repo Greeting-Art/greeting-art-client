fd2e5208973f2a843b20851da2b46084c3be6082
SamuelHGerber <henrylightfoot@gmail.com>
2021-06-18 11:11:04 -0700

    tests passing and updated

--- src/containers/ArtGenerator.test.jsx
+++ src/containers/ArtGenerator.test.jsx
@@ -6,13 +6,12 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from '../components/app/App';
 
-jest.mock('./ArtGenerator', () => () => <div>hey</div>);
+jest.mock('./ArtGenerator', () => () => <button>Randomize</button>);
 
 describe('random art generation container', () => {
   it('displays inputs + an image + buttons', async () => {
-
     const { container } = render(<App />);
-    expect(container.textContent).toMatch('hey');
+    expect(container.textContent).toMatch('Randomize');
 
     const P5Wrapper = function P5Wrapper(props) {
       var _this = _super.call(this, props) || this;
